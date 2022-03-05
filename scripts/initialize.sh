8e9d8aa52ef87592cca3cf685228093c2cf2819f
Haeun Kang <tejavascript@gmail.com>
2022-03-06 03:25:49 +0900

    fix: script

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -3,7 +3,10 @@ cd /home/ubuntu/DEVzine-port/server
 sudo apt install npm
 npm install
 npm install pm2@latest -g
+sudo apt update
 sudo apt install redis-server
+echo supervised systemd /etc/redis/redis.conf
+sudo systemctl restart redis.service
 sudo apt-get install authbind
 sudo touch /etc/authbind/byport/80
 sudo chown ubuntu /etc/authbind/byport/80
